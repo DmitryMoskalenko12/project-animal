ff1bcd51150222b10118d1738ac9cb5b0aa94bbc
Sergiy Khomchenko <dmtrmsklnk@gmail.com>
2022-10-23 19:49:21 +0300

    finish modal create

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,30 @@
 import './App.scss';
-import MainSection from '../mainSection/MainSection';
+/* import MainSection from '../mainSection/MainSection';
 import Barf from '../barf/Barf';
-import SetsZoov from '../setsZoov/SetsZoov';
+import SetsZoov from '../setsZoov/SetsZoov'; */
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+
+const Main = lazy(() => import('../mainSection/MainSection'));
+const Barf = lazy(() => import('../barf/Barf'));
+const SetsZoov = lazy(() => import('../setsZoov/SetsZoov'));
+
 function App() {
   return (
+    <Suspense fallback={<div style={{display: 'flex', justifyContent: 'center', marginTop: '20%'}}>...Загрузка</div>}>
     <Router>
-      <Routes>
-       <Route path='/' element={<MainSection/>}/>
-       <Route path='/barf' element={<Barf/>}/>
-       <Route path='/sets' element={<SetsZoov/>}/>
-      </Routes>  
-    </Router>
+          <Routes>
+          <Route path='/' element={<Main/>}/>
+          <Route path='/barf' element={<Barf/>}/>
+          <Route path='/sets' element={<SetsZoov/>}/>
+          </Routes>  
+        </Router>
+    </Suspense>
   );
 }
 
 export default App;
+
+
+
--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -94,3 +94,5 @@ return(
 )
 }
 export default Modal;
+
+
