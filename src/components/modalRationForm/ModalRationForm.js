4acd5620268389dcfa65eaa6313db5e6ddda3eaf
Sergiy Khomchenko <dmtrmsklnk@gmail.com>
2022-11-01 21:19:29 +0200

    add func in rationmodal form

--- src/components/modalRationForm/ModalRationForm.js
+++ src/components/modalRationForm/ModalRationForm.js
@@ -1,7 +1,34 @@
 import './modalRationForm.scss';
 import { useState } from 'react';
+import { useHttp } from '../../hooks/http.hook';
 
 const ModalRationForm = (props) =>{
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [addr, setAddr] = useState('');
+  const [weight, setWeight] = useState('');
+  const [check, setCheck] = useState('');
+
+  const [error, setError] = useState(false);
+  const [showContent, setShowContent] = useState(true);
+
+  const {request} = useHttp();
+  const price = props.data[props.id - 1].price;
+
+  const postData = () =>{
+   const formData = {count, name, phone, addr, weight, check, price};
+   request('http://localhost:3001/post', 'POST', JSON.stringify(formData))
+   .then(() => {
+     setCount(0); 
+     setName('');
+     setWeight(''); 
+     setPhone(''); 
+     setAddr(''); 
+     setCheck('');  
+     setTimeout(()=>{props.setFormModal(false); props.setModal(false)}, 2000)})
+    .catch(() => setError(true), setTimeout(()=>{props.setModal(false)}, 2000)) 
+  }
 
   return(
     <div className='modalrationform'>
@@ -11,21 +38,23 @@ const ModalRationForm = (props) =>{
           </div>
 
               <div className="modalrationform__wrapcontent">
+              <div onClick={() => props.setModal(false)} className="modalrationform__close">&times;</div>
               <div className="modalrationform__title">{props.data[props.id - 1].type} </div>
               <div className="modalrationform__meat">{props.data[props.id - 1].chick}</div>
 
-              <form className="modalrationform__form">
+              <form onSubmit={(e) => {postData(); e.preventDefault(); setShowContent(false)}} className="modalrationform__form">
               <div className="modalrationform__coll">
-                <input type="text" required placeholder='Имя' className="modalrationform__name" />
-                <input type="number" required placeholder='Телефон' className="modalrationform__phone" />
-                <input type="text" required placeholder='Адрес' className="modalrationform__addr" />
+                <input value={name} onChange = {(e)=> setName(e.target.value)} type="text" required placeholder='Имя' className="modalrationform__name" />
+                <input value={phone} onChange = {(e)=> setPhone(e.target.value)} type="number" required placeholder='Телефон' className="modalrationform__phone" />
+                <input value={addr} onChange = {(e)=> setAddr(e.target.value)} type="text" required placeholder='Адрес' className="modalrationform__addr" />
 
-                <select name="" id="" className="modalrationform__weight">
-                  <option value="">700 г</option>
-                  <option value="">500 г</option>
-                  <option value="">300 г</option>
-                  <option value="">150 г</option>
-                  <option value="">100 г</option>
+                <select value={weight} onChange = {(e)=> setWeight(e.target.value)} name="weight" className="modalrationform__weight">
+                  <option value="">Выбор веса</option>
+                  <option value="700">700 г</option>
+                  <option value="500">500 г</option>
+                  <option value="300">300 г</option>
+                  <option value="150">150 г</option>
+                  <option value="100">100 г</option>
                 </select>
               </div>
 
@@ -33,22 +62,22 @@ const ModalRationForm = (props) =>{
                <div className="modalrationform__calctitle">Количество:</div>
 
                <div className="modalrationform__plusminus">
-                <span className="modalrationform__plus">+</span>
-                <div className="modalrationform__state">1</div>
-                <span className="modalrationform__minus">-</span>
+                <span onClick={() => setCount(count => count + 1)} className="modalrationform__plus">+</span>
+                <div className="modalrationform__state">{count < 0 ? setCount(0) : count}</div>
+                <span onClick={() => setCount(count => count - 1)} className="modalrationform__minus">-</span>
                </div>
 
               </div>
               <div className="modalrationform__checkboxwrap">
-                  <input  type="checkbox" required name="checkboxform" id='modal__checkboxform'/>
+                  <input value={check} onChange = {(e)=> setCheck(e.target.value)}  type="checkbox" required name="checkboxform" id='modal__checkboxform'/>
                   <label htmlFor='modal__checkboxform' className="modalrationform__checkdescr">
                   Даю согласие на обработку персональных данных
                   </label>
               </div>
 
               <div className="modalrationform__butprice">
-              <div className="modalrationform__price">828грн</div>
-              <div className="modalrationform__butform">Оформить заказ</div>
+              <div className="modalrationform__price">{props.data[props.id - 1].price}</div>
+              <button className="modalrationform__butform">Оформить заказ</button>
               </div>
 
               </form>
--- src/components/modalRations/ModalRations.js
+++ src/components/modalRations/ModalRations.js
@@ -8,6 +8,7 @@ const ModalRations = (props) =>{
 
 const content = (
   <div className='modalrations__wrapall'>
+    <div onClick={() => props.setModal(false)} className="modalrations__close">&times;</div>
     <div className="modalrations__gridwrapp">
       <div className="modalrations__imgwrap">
         <img src={props.data[props.butid - 1].picture} alt="pict1" />
@@ -16,12 +17,12 @@ const content = (
         <div className="modalrations__title">{props.data[props.butid - 1].type} </div>
         <div className="modalrations__chick">{props.data[props.butid - 1].chick}</div>
         <div className="modalrations__vitamin">{props.data[props.butid - 1].descr}</div>
-        <div className="modalrations__include">Состав: Мякоть баранины (46,5%), мясо кролика (23,5%), кости
+        <div className="modalrations__include"><span>Состав:</span> Мякоть баранины (46,5%), мясо кролика (23,5%), кости
         кролика (10%), печень баранины (5%), почки бараньи (2,5%),
         рубец бараний (2,5%), кабачок (3%), брокколи (3%), семена
         конопли (2%), шпинат (1%), яблоко (1%), льняное масло,
         конопляное масло.</div>
-        <div className="modalrations__call">Пищевая ценность на 100г: 174.9 ккал, 
+        <div className="modalrations__call"> <span>Пищевая ценность на 100г:</span> 174.9 ккал, 
         белки 16.45, жиры 11.9, углеводы 0.60</div>
         <div className="modalrations__weight">{props.data[props.butid - 1].weight}</div>
         <button className="modalrations__button" onClick={() => setFormModal(true)}>Заказать</button>
@@ -33,10 +34,8 @@ const content = (
     <div onClick={() => props.setModal(false)} /* style={{display: props.modal ? 'block': 'none'}} */ className={props.modal ? 'modalrations active' : 'modalrations' }>
     <div onClick={(e) => e.stopPropagation()} className= "modalrations__content">
 
-      <div onClick={() => props.setModal(false)} className="modalrations__close">&times;</div>
-
      {
-   /*  error ? <div style={{textAlign: 'center',fontSize: '20px', marginTop: '80px', color: 'red'}}>Произошла ошибка</div>: showModal ? content : <ThanksModal/> */ formModal ? <ModalRationForm data = {props.data} id = {props.butid}/> : content
+   /*  error ? <div style={{textAlign: 'center',fontSize: '20px', marginTop: '80px', color: 'red'}}>Произошла ошибка</div>: showModal ? content : <ThanksModal/> */ formModal ? <ModalRationForm setModal = {props.setModal} setFormModal = {setFormModal} data = {props.data} id = {props.butid}/> : content
      }
 
     </div>
